a2297567c893d0c1c29f70180d3f6e1dc1a2e0f1
LSM <111512111+qqqlsm95@users.noreply.github.com>
2025-06-23 19:28:20 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int A, B;
+    
+    while(cin >> A >> B){
+        cout << A+B << '\n';
+    }
+    
+    return 0;
+}
