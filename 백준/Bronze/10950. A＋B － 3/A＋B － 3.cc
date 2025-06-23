78534bab013385828b6ef566f915559a68f4230e
LSM <111512111+qqqlsm95@users.noreply.github.com>
2025-06-23 19:24:49 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int N, A, B;
+
+    cin >> N;
+
+    for(int i=0; i<N ; ++i)
+    {
+        cin >> A >> B;
+        cout << A + B << endl;
+    }
+    
+    return 0;
+}
