3eea07ebb8877be7f0abe732c0923f2ad387b695
LSM <111512111+qqqlsm95@users.noreply.github.com>
2025-06-23 19:23:58 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    int prize;
+
+    if (a == b && b == c) {
+        prize = 10000 + a * 1000;
+    } 
+    
+    else if (a == b || a == c) {
+        prize = 1000 + a * 100;
+    } 
+    
+    else if (b == c) {
+        prize = 1000 + b * 100;
+    } 
+    
+    else {
+        prize = max({a, b, c}) * 100;
+    }
+
+    cout << prize << std::endl;
+    
+    return 0;
+}
